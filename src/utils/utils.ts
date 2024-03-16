0f7184453e0262c7783e43777a11134c50d65b7e
ulvy11 <152696191+ulvy11@users.noreply.github.com>
2024-03-16 16:27:13 +0100

    feat: add Utils class

---
+++ src/utils/utils.ts
@@ -0,0 +1,40 @@
+export interface ResultAndTabId<Result> {
+  tabId: number | undefined;
+  results: Array<
+    chrome.scripting.InjectionResult<chrome.scripting.Awaited<Result>>
+  >;
+}
+
+export class Utils {
+  private constructor() {}
+
+  public static logJsonStringified(object: any): void {
+    console.log(JSON.stringify(object, null, 5));
+  }
+
+  public static async executeScriptOnCurrentTab<Result>(
+    func: () => Result
+  ): Promise<ResultAndTabId<Result>> {
+    const tabData = await chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
+    const tabId = tabData[0].id;
+
+    let results: chrome.scripting.InjectionResult<
+      chrome.scripting.Awaited<Result>
+    >[] = [];
+
+    if (tabId) {
+      results = await chrome.scripting.executeScript({
+        target: { tabId },
+        func,
+      });
+    }
+
+    return {
+      tabId,
+      results,
+    };
+  }
+}
